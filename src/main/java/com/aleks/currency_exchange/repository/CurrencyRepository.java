0f4aff96050b030e5aaf7640fb4a72a9c65dfea1
aleksei <darklisik.darklisik@yandex.ru>
2025-03-09 15:35:21 +0300

    add new repository: CurrencyRepository.java

--- src/main/java/com/aleks/currency_exchange/repository/CurrencyRepository.java
+++ src/main/java/com/aleks/currency_exchange/repository/CurrencyRepository.java
@@ -2,18 +2,14 @@ package com.aleks.currency_exchange.repository;
 
 import com.aleks.currency_exchange.model.Currency;
 
-import java.sql.SQLException;
 import java.util.Collection;
 import java.util.Optional;
 
 public interface CurrencyRepository {
 
-    Collection<Currency> findAll() throws SQLException, ClassNotFoundException;
+    Collection<Currency> findAll();
 
-    Optional<Currency> findByCode(String code) throws SQLException, ClassNotFoundException;
+    Optional<Currency> findByCode(String code);
 
-    Optional<Currency> save(Currency currency) throws SQLException, ClassNotFoundException;
-
-    boolean deleteByCode(String code) throws SQLException, ClassNotFoundException;
-
+    Optional<Currency> save(Currency currency);
 }
