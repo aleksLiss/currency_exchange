ae098aa6b35cf19172de5178cc3ccef65394d97d
aleks <darklissik.darklissik@gmail.com>
2025-03-15 14:11:15 +0300

    add new view: ExchangeRateView.java

--- src/main/java/com/aleks/currency_exchange/view/ExchangeRateView.java
+++ src/main/java/com/aleks/currency_exchange/view/ExchangeRateView.java
@@ -1,15 +1,15 @@
-package com.aleks.currency_exchange.mapper;
+package com.aleks.currency_exchange.view;
 
 import com.aleks.currency_exchange.model.Currency;
 
-public class ExchangeRateMapper {
+public class ExchangeRateView {
 
     private int id;
     private Currency baseCurrency;
     private Currency targetCurrency;
     private double rate;
 
-    public ExchangeRateMapper(int id, Currency baseCurrency, Currency targetCurrency, double rate) {
+    public ExchangeRateView(int id, Currency baseCurrency, Currency targetCurrency, double rate) {
         this.id = id;
         this.baseCurrency = baseCurrency;
         this.targetCurrency = targetCurrency;
