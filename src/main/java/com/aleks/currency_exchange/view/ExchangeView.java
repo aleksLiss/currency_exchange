e68e38e863cb5d973521992ad25b71800814e5be
aleks <darklissik.darklissik@gmail.com>
2025-03-15 14:11:06 +0300

    add new view: ExchangeView.java

--- src/main/java/com/aleks/currency_exchange/view/ExchangeView.java
+++ src/main/java/com/aleks/currency_exchange/view/ExchangeView.java
@@ -1,10 +1,10 @@
-package com.aleks.currency_exchange.mapper;
+package com.aleks.currency_exchange.view;
 
 import com.aleks.currency_exchange.model.Currency;
 
 import java.math.BigDecimal;
 
-public class ExchangeMapper {
+public class ExchangeView {
 
     private Currency baseCurrency;
     private Currency targetCurrency;
@@ -12,7 +12,7 @@ public class ExchangeMapper {
     private double amount;
     private BigDecimal convertedAmount;
 
-    public ExchangeMapper(Currency baseCurrency, Currency targetCurrency, double rate, double amount, BigDecimal convertedAmount) {
+    public ExchangeView(Currency baseCurrency, Currency targetCurrency, double rate, double amount, BigDecimal convertedAmount) {
         this.baseCurrency = baseCurrency;
         this.targetCurrency = targetCurrency;
         this.rate = rate;
