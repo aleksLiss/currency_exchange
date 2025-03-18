30cbc2b4852f6b67393f4958fc7a6638b67b8e74
aleksei <darklisik.darklisik@yandex.ru>
2025-03-18 22:43:29 +0300

    change Validator.java to ParametersValidator.java

--- src/main/java/com/aleks/currency_exchange/validator/ParametersValidator.java
+++ src/main/java/com/aleks/currency_exchange/validator/ParametersValidator.java
@@ -2,7 +2,8 @@ package com.aleks.currency_exchange.validator;
 
 import java.util.Map;
 
-public interface Validator {
+public interface ParametersValidator {
 
     boolean isValidParameters(Map<String, String> parameters);
+
 }
