0b30da0b7142932aef7f17d7c6846ba8cd3f1430
aleks <darklissik.darklissik@gmail.com>
2025-03-26 20:15:38 +0300

    change fields from 'Currency' to 'CurrencyView' into ExchangeView.java

--- src/main/java/com/aleks/currency_exchange/view/ExchangeView.java
+++ src/main/java/com/aleks/currency_exchange/view/ExchangeView.java
@@ -1,18 +1,17 @@
 package com.aleks.currency_exchange.view;
 
-import com.aleks.currency_exchange.model.Currency;
 
 import java.math.BigDecimal;
 
 public class ExchangeView {
 
-    private Currency baseCurrency;
-    private Currency targetCurrency;
+    private CurrencyView baseCurrency;
+    private CurrencyView targetCurrency;
     private BigDecimal rate;
     private double amount;
     private BigDecimal convertedAmount;
 
-    public ExchangeView(Currency baseCurrency, Currency targetCurrency, BigDecimal rate, double amount, BigDecimal convertedAmount) {
+    public ExchangeView(CurrencyView baseCurrency, CurrencyView targetCurrency, BigDecimal rate, double amount, BigDecimal convertedAmount) {
         this.baseCurrency = baseCurrency;
         this.targetCurrency = targetCurrency;
         this.rate = rate;
