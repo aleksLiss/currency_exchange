ced50ff12775021fc7d52cb6614f212ca129c62a
aleks <darklissik.darklissik@gmail.com>
2025-03-26 20:14:50 +0300

    add new method 'getMessage' into ExceptionView.java

--- src/main/java/com/aleks/currency_exchange/view/ExceptionView.java
+++ src/main/java/com/aleks/currency_exchange/view/ExceptionView.java
@@ -1,19 +1,18 @@
 package com.aleks.currency_exchange.view;
 
-import com.aleks.currency_exchange.exception.ExchangeException;
 
 public class ExceptionView {
 
     private String message;
-
-    public ExceptionView(String message) {
-        this.message = message;
-    }
 
     public ExceptionView() {
     }
 
     public void setMessage(String message) {
         this.message = message;
+    }
+
+    public String getMessage() {
+        return this.message;
     }
 }
